b67391f421aaa4a9ef71bdf4a3d070602e0ae6da
Collins <jbcollins254@gmail.com>
2024-05-09 19:59:03 +0300

    updates like button

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import './FeedbackForm.css';
 
@@ -23,13 +29,13 @@ const FeedbackForm = () => {
       [e.target.name]: e.target.value
     });
   };
-
-  const handleSubmit = (e) => {
+  const newComment = { ...formData, like: 0 }
+  const handleSubmit = (e) => { 
     e.preventDefault();
     
     fetch("http://localhost:3000/comments", {
       method: 'POST',
-      body: JSON.stringify(formData),
+      body: JSON.stringify(newComment),
       headers: {
         'Content-Type': 'application/json'
       }
@@ -44,64 +50,97 @@ const FeedbackForm = () => {
     });
   };
 
+  const getRandomColor = () => {
+   
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+  };
+
+
+  const handleLike = (index) => {
+    const updatedComments = [...comments];
+    const commentToUpdate = updatedComments[index];
+  
+    
+
+    commentToUpdate.like++;
+  
+  
+    fetch(`http://localhost:3000/comments/${commentToUpdate.id}`, {
+      method: 'PUT', 
+      body: JSON.stringify(commentToUpdate),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(updatedComment => {
+    
+      updatedComments[index] = updatedComment;
+      setComments(updatedComments);
+    })
+    .catch(error => {
+      console.error('Error updating like count:', error);
+    });
+  };
+  
   return (
     <div className="container">
       <div className="formContainer">
         <h2 className="title">Comment Form</h2>
         <form onSubmit={handleSubmit}>
-          <div className="inputGroup">
+        <div className="inputGroup">
        
-            <input 
-              className="input inputLarge" 
-              type="text" 
-              id="name" 
-              placeholder="User Name..."
-              name="name" 
-              value={formData.name} 
-              onChange={handleChange} 
-              required 
-            />
-          </div>
-          <div className="inputGroup">
-            
-            <input 
-              className="input inputLarge" 
-              type="text" 
-              id="avatar" 
-              placeholder="Profile..."
-              name="avatar" 
-              value={formData.avatar} 
-              onChange={handleChange} 
-            />
-          </div>
-          <div className="inputGroup">
-           
-            <input 
-              className="input inputLarge" 
-              type="text" 
-              id="photo" 
-              placeholder="Photo..."
-              name="photo" 
-              value={formData.photo} 
-              onChange={handleChange} 
-              required 
-            />
-          </div>
-         
-          <div className="inputGroup">
-      
-            <textarea 
-              className="textarea textareaLarge" 
-              id="message" 
-              placeholder="Comment..."
-              name="message" 
-              value={formData.message} 
-              onChange={handleChange} 
-              rows="4" 
-              required 
-            />
-          </div>
-          <button className="button" type="submit">Submit</button>
+       //             <input 
+                     className="input inputLarge" 
+                     type="text" 
+                     id="name" 
+                     placeholder="User Name..."
+                     name="name" 
+                     value={formData.name} 
+                     onChange={handleChange} 
+                     required 
+                   />
+                 </div>
+                 <div className="inputGroup">
+                   
+                   <input 
+                     className="input inputLarge" 
+                     type="text" 
+                     id="avatar" 
+                     placeholder="Profile..."
+                     name="avatar" 
+                     value={formData.avatar} 
+                     onChange={handleChange} 
+                   />
+                 </div>
+                 <div className="inputGroup">
+                  
+                   <input 
+                     className="input inputLarge" 
+                     type="text" 
+                     id="photo" 
+                     placeholder="Photo..."
+                     name="photo" 
+                     value={formData.photo} 
+                     onChange={handleChange} 
+                     required 
+                   />
+                 </div>
+                
+                 <div className="inputGroup">
+             
+                   <textarea 
+                     className="textarea textareaLarge" 
+                     id="message" 
+                     placeholder="Comment..."
+                     name="message" 
+                     value={formData.message} 
+                     onChange={handleChange} 
+                     rows="4" 
+                     required 
+                   />
+                 </div>
+                 <button className="button" type="submit">Submit</button>
         </form>
       </div>
 
@@ -113,11 +152,20 @@ const FeedbackForm = () => {
               <li key={index} className="commentItem">
                 <div className="commentCard">
                   <div className="userProfile">
-                    <img src={comment.avatar} alt="User Avatar" className="avatar" />
+                    {comment.avatar ? (
+                      <img src={comment.avatar} alt="User Avatar" className="avatar" />
+                    ) : (
+                      <div className="avatar" style={{ backgroundColor:getRandomColor() }}>
+                        {comment.name.charAt(0).toUpperCase()}
+                      </div>
+                    )}
                     <span className="userName">{comment.name}</span>
                   </div>
                   <p className="commentMessage">{comment.message}</p>
-                  <img src={comment.photo} alt="Comment Photo" className="commentPhoto" />
+                  {comment.photo && <img src={comment.photo} alt="Comment Photo" className="commentPhoto" />}
+                  <button onClick={() => handleLike(index)}  className='like'>
+                     <span className="like-icon"> ❤️</span> {comment.like}
+                  </button>
                 </div>
               </li>
             ))}
@@ -129,3 +177,6 @@ const FeedbackForm = () => {
 };
 
 export default FeedbackForm;
+
+
+
